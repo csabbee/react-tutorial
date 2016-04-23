541bfb24f87cdbd1316dade22b5ae8eafb923e17
Csaba Valyi <csaba.valyi1@gmail.com>
2016-04-23 22:28:42 +0200

    markup rendering

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -32,13 +32,18 @@ class CommentForm extends React.Component {
 }
 
 class Comment extends React.Component {
+    rawMarkup() {
+        var rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+        return { __html: rawMarkup };
+    }
+
     render() {
         return (
             <div className="comment">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                {this.props.children}
+                <span dangerouslySetInnerHtml={this.rawMarkup()} />
             </div>
         );
     }
