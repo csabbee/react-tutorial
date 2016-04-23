e37fe08656020f4f24a80131de9aa42388910612
Csaba Valyi <csaba.valyi1@gmail.com>
2016-04-23 22:18:16 +0200

    comment list, comment form component created

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,12 +1,35 @@
 class CommentBox extends React.Component {
     render() {
         return (
-            <div class="commentBox">
+            <div className="commentBox">
                 Hello world! I am a comment box.
+                <CommentList/>
+                <CommentForm/>
             </div>
         )
     }
-};
+}
+
+class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="commentList">
+                Hello world! I am a comment list.
+            </div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">
+                Hello world! I am a comment form.
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(
     <CommentBox/>,
