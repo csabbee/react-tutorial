8731ebc2213bcfa37b2f0f03f55c4fe513d1d24d
Csaba Valyi <csaba.valyi1@gmail.com>
2016-04-23 22:18:45 +0200

    comment component created

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -30,6 +30,18 @@ class CommentForm extends React.Component {
     }
 }
 
+class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <CommentBox/>,
